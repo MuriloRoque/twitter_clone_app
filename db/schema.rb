800c37e92d756d73b8ded433b25bdb42b09b8cd0
Murilo Roque <muriloengqui@gmail.com>
2020-05-07 10:37:49 -0300

    README update and lint errors fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130000452) do
+ActiveRecord::Schema.define(version: 2020_05_07_110232) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
